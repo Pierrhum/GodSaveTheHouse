988adf8e3c190b512082195eb1a6739d87658975
82L <16747176+82L@users.noreply.github.com>
2023-09-12 00:20:59 +0200

    added test version of aduino script

---
+++ arduino_scripts/godSaveTheHouse_altctrl/godSaveTheHouse_altctrl.ino
@@ -0,0 +1,154 @@
+#define BUZZER_PIN 3
+#define TRIGGER_PIN 13
+#define ECHO_PIN 12
+#define LED_1_PIN 10
+#define LED_2_PIN 9
+#define LED_3_PIN 8
+#define BUTTON_PIN A0
+
+float distance;
+float duration;
+
+
+int last_pin_used = LED_BUILTIN;
+unsigned long current_time=0; 
+unsigned long tone_duration = 300;
+unsigned long last_time_buzzed = 0;
+
+unsigned long duration_for_next_state = 0;
+unsigned long time_since_previous_state = 0;
+
+unsigned long current_time_micros;
+unsigned long duration_micro_for_next_state = 0;
+unsigned long time_micro_since_previous_state = 0;
+
+int btnPressedPrevValue = 0;
+bool btnPressed = false;
+bool btnIsDown = false;
+enum Sensor_State {
+  START_SEND,
+  STOP_SEND,
+  START_RECEIVE,
+  STOP_RECEIVE
+}; 
+Sensor_State current_sensor_state = START_SEND;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(TRIGGER_PIN,OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_1_PIN, OUTPUT);
+
+  pinMode(LED_2_PIN, OUTPUT);
+
+  pinMode(LED_3_PIN, OUTPUT);
+
+   //Init btn
+ 	pinMode(BUTTON_PIN, INPUT_PULLUP);
+  btnPressedPrevValue = analogRead(BUTTON_PIN);
+
+
+}
+
+void loop() {
+  current_time = millis();
+  current_time_micros = micros();
+  distanceSensorTest();
+  testPushBtn();
+}
+void distanceSensorTest(){
+
+  // put your main code here, to run repeatedly:
+  if(current_sensor_state == START_SEND){
+    digitalWrite(TRIGGER_PIN, LOW);
+    
+   // Wait for 2 microseconds
+   time_micro_since_previous_state = current_time_micros;
+    duration_micro_for_next_state = 2;
+    current_sensor_state = STOP_SEND;
+  }
+  if(current_sensor_state == STOP_SEND and isDelayMicroTimePassed()){
+
+ 
+    digitalWrite(TRIGGER_PIN, HIGH);
+   // Wait for 10 microseconds
+    time_micro_since_previous_state = current_time_micros;
+    duration_micro_for_next_state = 10;
+    current_sensor_state = START_RECEIVE;
+  }
+ 
+  if( current_sensor_state == START_RECEIVE and isDelayMicroTimePassed()){
+    digitalWrite(TRIGGER_PIN, LOW);
+    duration=pulseIn(ECHO_PIN, HIGH);
+    distance=(duration*0.034)/2;
+  
+    if(distance > 15 and distance < 25){
+      triggerLed(LED_1_PIN);
+    }
+    if(distance > 40 and distance < 50){
+      triggerLed(LED_2_PIN);
+    }
+    if(distance > 65 and distance < 75){
+      triggerLed(LED_3_PIN);
+    }
+    turnOffLed();
+    current_sensor_state = STOP_RECEIVE;
+    time_since_previous_state = current_time;
+    duration_for_next_state = 100;
+  }
+  
+ 
+  if(current_sensor_state == STOP_RECEIVE and isDelayTimePassed() )
+  {
+    current_sensor_state = START_SEND;
+  }
+
+}
+
+bool isDelayTimePassed(){
+
+  return ((time_since_previous_state + duration_for_next_state)<current_time);
+}
+
+bool isDelayMicroTimePassed(){
+
+  return ((time_micro_since_previous_state + duration_micro_for_next_state)<current_time_micros);
+}
+
+void  triggerLed(int pin){
+  if (pin == 0){
+    pin = LED_BUILTIN;
+  }
+
+  if(last_time_buzzed + tone_duration < current_time){
+    digitalWrite(last_pin_used, LOW);
+    digitalWrite(pin, HIGH);
+    last_pin_used = pin;
+    last_time_buzzed = current_time;
+  }
+}
+void turnOffLed(){
+  if(last_time_buzzed + tone_duration < current_time){
+    digitalWrite(last_pin_used, LOW);
+  }
+
+}
+
+
+void testPushBtn() {
+  // Read pushbutton
+  int btn = analogRead(BUTTON_PIN);
+  btnPressed = false;
+  if (btn < 200 && !btnIsDown){
+    btnPressed = true;
+    btnIsDown = true;
+     triggerLed(LED_BUILTIN);
+  }
+  else if(btn > 500)
+  {
+    btnIsDown = false;
+  }
+ 
+}
